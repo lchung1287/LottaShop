d95fe0a233ff17c04a6aa018ae31936abcdae774
Kelly Wang <kellywang011@gmail.com>
2021-11-29 16:40:26 -0600

    added login vc and sign up vc

--- GroceryApp/LoginViewController.swift
+++ GroceryApp/LoginViewController.swift
@@ -13,7 +13,20 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func signUp(_ sender: Any) {
-        
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 100.0/255.0, blue: 110.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
     }
 }
--- GroceryApp/SignupViewController.swift
+++ GroceryApp/SignupViewController.swift
@@ -15,6 +15,22 @@ class SignupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 100.0/255.0, blue: 110.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
 
     /*
     // MARK: - Navigation
