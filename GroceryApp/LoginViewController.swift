ed7d0ac566660b8e2c3f30f8f2993b0777c0c8a4
Kelly Wang <kellywang011@gmail.com>
2021-11-29 15:00:21 -0600

    test1

---
+++ GroceryApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  GroceryApp
+//
+//  Created by Kelly Wang on 11/16/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+        
+    }
+}
---
+++ GroceryApp/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  GroceryApp
+//
+//  Created by Kelly Wang on 11/16/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+//    var tableView: UITableView!
+    
+    @IBOutlet weak var logoutButton: UIBarButtonItem!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    
+    //login button right now
+    
+    // configure table view function
+//    func makeTableView(){
+//        tableView = UITableView()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.rowHeight = 60
+//
+//        tableView.register(SettingsCell.self, forCellReuseIdentifier: reuseIdentifier)
+//        view.addSubview(tableView)
+//        tableView.frame = view.frame
+//    }
+    
+}
